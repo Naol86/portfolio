fd657e7581745c0a6d59464d06aff67c547acad3
naol86 <naol.kasinet@a2sv.org>
2024-12-23 14:30:05 +0300

    Enhance Achievement component styling for improved visual appeal and readability

--- src/components/Achievement.tsx
+++ src/components/Achievement.tsx
@@ -4,19 +4,21 @@ import Image from "next/image";
 
 export default function Achievement({ achievement }: { achievement: aType }) {
   return (
-    <div className="rounded-[7px] border-4 border-blue-400 h-full flex flex-col group">
+    <div className="rounded-[10px] bg-blue-200 bg-opacity-5 backdrop-blur-md h-full flex flex-col group">
       <div className="relative w-full aspect-[8/4] overflow-hidden">
         <Image
           src={achievement.image}
           fill
           alt={achievement.title}
-          className="object-cover rounded-lg w-full group-hover:scale-105 transition-transform duration-300"
+          className="object-cover rounded-[10px] w-full group-hover:scale-105 transition-transform duration-300"
         />
         <div className="absolute inset-0 bg-black bg-opacity-30 group-hover:bg-opacity-10 duration-300 rounded-lg"></div>
       </div>
-      <div className="bg-blue-200 grow bg-opacity-5 backdrop-blur-md px-2 py-2">
-        <h3 className="text-base font-semibold">{achievement.title}</h3>
-        <p className="text-sm font-medium text-gray-400 py-1">
+      <div className="grow px-2 py-2 rounded-b-[10px]">
+        <h3 className="text-base font-semibold text-white">
+          {achievement.title}
+        </h3>
+        <p className="text-xs font-medium text-gray-400 py-1">
           {achievement.description}
         </p>
       </div>
